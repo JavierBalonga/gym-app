0a280dc860d016b04105e3c27cabab0299e764ec
JavierBalonga <javierbalonga@gmail.com>
2023-11-03 20:08:33 -0300

    chore(*): add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,25 @@
+// prettier-ignore
+const config = {
+  arrowParens: 'always',
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    // 'prettier-plugin-tailwindcss'
+  ],
+  // tailwindConfig: './tailwind.config.js',
+  importOrder: [
+    '^react$',
+    '^[^\\.]+$',
+    '',
+    '^\\.{2}(.*)$',
+    '^\\.{1}(.*)$',
+    '^(.*)$'
+  ],
+};
+
+module.exports = config;
