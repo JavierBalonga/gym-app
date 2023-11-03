f17f1469c55b1a7403628f8e08382c1191dd0705
JavierBalonga <javierbalonga@gmail.com>
2023-11-03 20:42:15 -0300

    chore(*): add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
