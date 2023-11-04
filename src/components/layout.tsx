08668805848a5003d9d4e3e4e05fb7297be6b51e
JavierBalonga <javierbalonga@gmail.com>
2023-11-03 21:53:06 -0300

    feat(layout): add MdoeToggle to header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
 
 export default function Layout() {
   return (
     <>
       <header className="flex flex-row justify-center">
-        <div className="grow max-w-4xl">Header</div>
+        <div className="grow max-w-4xl flex flex-row justify-between py-4">
+          <Link to="/">
+            <Button variant="link" className="text-2xl font-bold">
+              Gym-App
+            </Button>
+          </Link>
+
+          <ModeToggle />
+        </div>
       </header>
       <main className="grow flex flex-col items-center">
         <Outlet />
       </main>
-      <footer className="flex flex-row justify-center">
-        <div className="grow max-w-4xl">Footer</div>
-      </footer>
     </>
   );
 }
