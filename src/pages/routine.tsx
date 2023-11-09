49a657f5b39ac37cd7be938a91d115a14339d1e9
JavierBalonga <javierbalonga@gmail.com>
2023-11-08 21:10:00 -0300

    feat(*): add RoutineForm first aproach

--- src/pages/routine.tsx
+++ src/pages/routine.tsx
@@ -1,19 +1,22 @@
 import { ChevronLeft } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
+import RoutineForm from '../components/routine-form';
 import Section from '../components/section';
 import { Button } from '../components/ui/button';
 
 export default function CreateRoutinePage() {
   const navigate = useNavigate();
+
   return (
-    <Section>
+    <Section className="gap-16">
       <div className="flex flex-row items-center gap-4">
         <Button size="icon" variant="outline" onClick={() => navigate(-1)}>
           <ChevronLeft />
         </Button>
-        <h1 className="text-3xl font-bold">Create Routine</h1>
+        <h1 className="text-3xl font-bold">Nueva Rutina</h1>
       </div>
+      <RoutineForm />
     </Section>
   );
 }
