21a7c730e61091a24d6ff4d7b4052cfc3448a41f
JavierBalonga <JavierBalonga@users.noreply.github.com>
2023-11-14 01:10:23 +0000

    deploy: cd5cb4799fab36cb515d49929aa6fc374eb3f248

--- assets/not-found-c659ce43.js
+++
@@ -1,1 +0,0 @@
-import{j as t,S as o}from"./index-79062446.js";function n(){return t.jsx(o,{children:"404"})}export{n as default};
---
+++ assets/not-found-cf3d71df.js
@@ -0,0 +1,1 @@
+import{j as t,S as o}from"./index-a0478df8.js";function n(){return t.jsx(o,{children:"404"})}export{n as default};
