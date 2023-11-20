525f6f18f6960f2e87983b41aff7793fb25040fd
JavierBalonga <javierbalonga@gmail.com>
2023-11-20 16:18:27 -0300

    fix(routine-form): remove unnecesary deps

--- src/components/business/routine-form/routine-form.tsx
+++ src/components/business/routine-form/routine-form.tsx
@@ -1,13 +1,5 @@
 import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+import { Form } from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
